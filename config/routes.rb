0fa0d948da8a9f7bf6fa9489afffc31adfe71c42
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2017-11-24 11:29:20 +0100

    Images

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+mount Attachinary::Engine => "/attachinary"
 
 root to: 'cocktails#index'
 
@@ -8,3 +9,4 @@ end
 resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
